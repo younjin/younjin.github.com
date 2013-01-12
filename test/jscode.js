d38e0cd177b03fa4299ed1b31bcb08dee0f90f02
younjin <me@kimyounjin.com>
2013-01-12 13:21:04 -0500

    DOING IT LIVE

--- test/jscode.js
+++ test/jscode.js
@@ -53,3 +53,16 @@ $('div.btn-group .btn').click(function(){
   $(this).find('input:button').attr('checked', true);
   selection = $('input[name=radio-btn-ctrl]:checked').val();
 });
+
+function postmessage() {
+FB.login(function(response) {
+   if (response.authResponse) {
+     console.log('Welcome!  Fetching your information.... ');
+     FB.api('/me', function(response) {
+       console.log('Good to see you, ' + response.name + '.');
+     });
+   } else {
+     console.log('User cancelled login or did not fully authorize.');
+   }
+ }, {scope: 'publish_stream'});
+}
