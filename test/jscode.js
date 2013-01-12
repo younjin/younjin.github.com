aec7ac673b1b83a493b7a80e0870b1bb46f58aae
younjin <me@kimyounjin.com>
2013-01-12 14:47:41 -0500

    #

--- test/jscode.js
+++ test/jscode.js
@@ -59,7 +59,7 @@ console.log ('s');
 FB.login(function(response) {
    if (response.authResponse) {
      console.log('Welcome!  Fetching your information.... ');
-     $("#friendSelector").jfmfs();
+     $("#friendselector").jfmfs();
      FB.api('/me', function(response) {
        console.log('Good to see you, ' + response.name + '.');
      });
@@ -73,7 +73,7 @@ FB.login(function(response) {
 $('#b6').click(function() {
 console.log ('t');
 var friendSelector = $("#jfmfs-container").data('jfmfs');
-var friendIds = friendSelector.getSelectedIds();
+var friendIds = friendselector.getSelectedIds();
 var msgs = $("#final").html();
 FB.api('/' + friendIds[0] + '/feed', 'post', {message: msgs}, function(response)
             {
